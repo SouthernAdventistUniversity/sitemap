de2c728b09b2560b349eeb753aa534ee57cc203d
Isaac Braun <isaacbraun2000@gmail.com>
2024-06-11 15:51:19 -0400

    feat: moved root node to correct place

--- xml-pages-sitemap.php
+++ xml-pages-sitemap.php
@@ -121,12 +121,10 @@ class Joost_XML_Sitemap_PHP {
 			// Check if this file needs to be ignored, if so, skip it.
       $paths_string = '#(' . implode(')|(', $this->ignore) . ')#';
       $utf8_file = mb_convert_encoding($file, 'UTF-8', 'ISO-8859-1');
-      $this->debug_output .= "<document>";
       if (preg_match($paths_string, $utf8_file, $out)) {
         $this->debug_output .= '<p>' . $utf8_file.' matched ' . $out[0] . '</p><br>' . PHP_EOL;
         continue;
       }
-      $this->debug_output .= "</document>";
 
 			if ( $this->recursive && is_dir( $dir . $file ) ) {
 				$this->parse_dir( $dir . $file . '/', $url . $file . '/' );
@@ -182,7 +180,9 @@ class Joost_XML_Sitemap_PHP {
 		/*if ( ! empty( $this->xsl ) ) {
 			echo '<?xml-stylesheet type="text/xsl" href="' . $this->url . $this->xsl . '"?>' . PHP_EOL;
     }*/
+    $this->debug_output .= "<document>";
     echo $this->debug_output;
+    $this->debug_output .= "</document>";
 		/* echo '<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">' . PHP_EOL;
 		echo $this->output;
 		echo '</urlset>' . PHP_EOL; */
