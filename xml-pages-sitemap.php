b37342bbbd09774f4a715687c7572912db1bf1c9
Isaac Braun <isaacbraun2000@gmail.com>
2024-06-11 12:02:55 -0400

    feat: new ignore matching test

--- xml-pages-sitemap.php
+++ xml-pages-sitemap.php
@@ -117,9 +117,17 @@ class Joost_XML_Sitemap_PHP {
 
 		while ( false !== ( $file = readdir( $handle ) ) ) {
 			// Check if this file needs to be ignored, if so, skip it.
-			if ( in_array( utf8_encode( $file ), $this->ignore ) ) {
-				continue;
-			}
+			// if ( in_array( utf8_encode( $file ), $this->ignore ) ) {
+			// 	continue;
+			// }
+
+      foreach($this->ignore AS $v) {
+        $utf8_file = mb_convert_encoding($file, 'UTF-8', 'ISO-8859-1');
+        if (preg_match('~'.$v.'~', $utf8_file)) {
+            // echo $v.' matched'; break;
+          continue;
+        }
+      }
 
 			if ( $this->recursive && is_dir( $dir . $file ) ) {
 				$this->parse_dir( $dir . $file . '/', $url . $file . '/' );
